6af7d47e17dc3fa46f03ddda071fdb3f05980163
veeranjaneyulu12170 <92072387+veeranjaneyulu12170@users.noreply.github.com>
2024-02-09 11:43:52 +0530

    Create NLP_EX-1.2.py

---
+++ NLP_EX-1.2.py
@@ -0,0 +1,15 @@
+## REG_X(printing digits from words)
+nlp = spacy.load('en_core_web_sm')
+
+sentence = "There are 3 apples and 5 oranges on the table."
+
+doc = nlp(sentence)
+
+numbers=[]
+for token in doc:
+    if token.is_digit:
+        numbers.append(token.text)
+
+print("Numbers in the sentence:", numbers)
+
+
