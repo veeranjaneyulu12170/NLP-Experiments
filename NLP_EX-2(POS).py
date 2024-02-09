ab6557b1486c7cf461211fcc443c3fc60e7416c5
veeranjaneyulu12170 <92072387+veeranjaneyulu12170@users.noreply.github.com>
2024-02-09 11:47:10 +0530

    Create NLP_EX-2(POS).py

---
+++ NLP_EX-2(POS).py
@@ -0,0 +1,20 @@
+nlp = spacy.load('en_core_web_sm')
+sentence = "I like coffee, but I don't like tea."
+
+doc = nlp(sentence)
+
+split_sentences = []
+current_sentence = []
+
+for token in doc:
+    if token.text.lower() in ['and', 'but', 'or', 'nor', 'for', 'so', 'yet']:
+        split_sentences.append(' '.join(current_sentence))
+        current_sentence = []
+    else:
+        current_sentence.append(token.text)
+
+split_sentences.append(' '.join(current_sentence))
+
+
+for i, sentence in enumerate(split_sentences):
+    print(f"Sentence {i + 1}: {sentence}")
