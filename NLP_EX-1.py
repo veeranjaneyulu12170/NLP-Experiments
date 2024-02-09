714e5cdc8f9887285eac5dbc137c3e9e30f585ef
veeranjaneyulu12170 <92072387+veeranjaneyulu12170@users.noreply.github.com>
2024-02-09 11:43:06 +0530

    Update NLP_EX-1.1.py
    -->Printing words from sentences using NLP

--- NLP_EX-1.py
+++ NLP_EX-1.py
@@ -9,38 +9,3 @@ doc = nlp(text)
 for token in doc:
     print(f"Token: {token.text}")
 
-## REG_X(printing digits from words)
-nlp = spacy.load('en_core_web_sm')
-
-sentence = "There are 3 apples and 5 oranges on the table."
-
-doc = nlp(sentence)
-
-numbers=[]
-for token in doc:
-    if token.is_digit:
-        numbers.append(token.text)
-
-print("Numbers in the sentence:", numbers)
-
-##Dividing Two sentences
-nlp = spacy.load('en_core_web_sm')
-sentence = "I like coffee, but I don't like tea."
-
-doc = nlp(sentence)
-
-split_sentences = []
-current_sentence = []
-
-for token in doc:
-    if token.text.lower() in ['and', 'but', 'or', 'nor', 'for', 'so', 'yet']:
-        split_sentences.append(' '.join(current_sentence))
-        current_sentence = []
-    else:
-        current_sentence.append(token.text)
-
-split_sentences.append(' '.join(current_sentence))
-
-
-for i, sentence in enumerate(split_sentences):
-    print(f"Sentence {i + 1}: {sentence}")
